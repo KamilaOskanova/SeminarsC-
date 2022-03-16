8abca4ec63197f62de5714f01b7b391b2d36eb70
«Kamila» <koskanova@outlook.com>
2022-03-17 00:20:36 +0300

    The second exercise added

---
+++ Seminar1/Exercise002/Program.cs
@@ -0,0 +1,21 @@
+﻿int number1;
+int number2;
+
+Console.WriteLine("Enter the first number: ");
+number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second number: ");
+number2 = Convert.ToInt32(Console.ReadLine());
+
+int squared_number2 = number2 * number2;
+
+Console.WriteLine("Is the first number the square of the second number?");
+
+if(number1 == squared_number2)
+{
+    Console.WriteLine("Yes, it is"); // cответствие квадрата второго числа первому числу
+}
+else
+{
+    Console.WriteLine("No, it is not");
+}
