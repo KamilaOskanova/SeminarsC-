db66f4b6e067fc1cf70a3ff07d6dbb840954c603
«Kamila» <koskanova@outlook.com>
2022-03-17 02:22:19 +0300

    The fourth task added

---
+++ Seminar1/Homework/Task004/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 4: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+Console.WriteLine("Введите натуральное число больше 1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int value = 2;
+
+while(value <= number)
+{
+    Console.Write(value + " ");
+    value +=2;
+}
+
+if (number <= 1)
+{
+    Console.WriteLine("Пожалуйста, введите число больше 1");
+}
