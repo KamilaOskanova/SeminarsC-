c30eaf409a61d67a099b7063429e6419df4764b1
«Kamila» <koskanova@outlook.com>
2022-03-17 00:22:28 +0300

    The third exercise added

---
+++ Seminar1/Exercise003/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Enter the number: ");
+int weeknumber = Convert.ToInt16(Console.ReadLine());
+
+if (weeknumber == 1)
+{
+    Console.WriteLine("Monday");
+}
+
+if (weeknumber == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+
+if (weeknumber == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+
+if (weeknumber == 4)
+{
+    Console.WriteLine("Thursday");
+}
+
+if (weeknumber == 5)
+{
+    Console.WriteLine("Friday");
+}
+
+if (weeknumber == 6)
+{
+    Console.WriteLine("Saturday");
+}
+
+if (weeknumber == 7)
+{
+    Console.WriteLine("Sunday");
+}
