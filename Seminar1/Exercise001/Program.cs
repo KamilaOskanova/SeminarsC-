96459f5156a6b65becbc2d243eb968dda7f1641a
«Kamila» <koskanova@outlook.com>
2022-03-17 00:16:55 +0300

    The first exercise added

---
+++ Seminar1/Exercise001/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите целое число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+int squared_value = value * value;
+
+Console.Write("Квадрат данного числа равен ");
+Console.WriteLine(squared_value);
