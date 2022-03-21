22b312dff7c58796ed078c7c6c10a19ef7f4e42a
«Kamila» <koskanova@outlook.com>
2022-03-21 03:02:22 +0300

    Added the second exercise with method from Seminar 2

---
+++ Seminar2/Exercise002/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа.
+
+int Delete2Digit (int num)
+{
+    int n1 = num / 100 * 10;
+    int n2 = num % 10;
+    return n1 + n2;
+}
+
+int number = new Random().Next(100, 999);
+Console.WriteLine(number + " --> " + Delete2Digit(number));
