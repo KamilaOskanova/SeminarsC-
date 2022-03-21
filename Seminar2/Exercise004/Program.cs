a8b22556ce8225d0294b157cf7e187b4f398b18c
«Kamila» <koskanova@outlook.com>
2022-03-21 03:21:21 +0300

    Added the fourth exercise with method from Seminar 2

---
+++ Seminar2/Exercise004/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число, а затем еще два числа,
+// и проверяет, кратно ли первое число двум другим.
+
+void multiple(int num1, int num2, int num3)
+{
+    if (num1 % num2 == 0 && num1 % num3 == 0)
+    {
+        Console.WriteLine("Yes, " + num1 + " is multiple of " + num2 + ", " + num3);
+    }
+    else
+    {
+        Console.WriteLine("No, " + num1 + " is not multiple of " + num2 + ", " + num3);
+    }
+}
+
+Console.WriteLine( "Enter the first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine( "Enter the second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine( "Enter the third number: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+multiple (number1, number2, number3);
