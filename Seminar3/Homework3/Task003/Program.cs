ac1bec5e172499da9db6cde331bad3ea50e9e957
«Kamila» <koskanova@outlook.com>
2022-03-24 03:05:28 +0300

    Added the third task about cubes from Homework 3

---
+++ Seminar3/Homework3/Task003/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+double GetCubes (int a)
+{
+    int index = 1;
+    double cube1 = 1;
+    while (index <= a)
+    {
+        cube1 = Math.Pow(index, 3);
+        Console.Write(" " + cube1 + " ");
+        index++;
+    }
+    return cube1;
+}
+
+
+Console.WriteLine ("Enter the natural number: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+if (numb <= 0)
+{
+    Console.WriteLine("Try again! You can only enter the numbers more than zero. ");
+}
+else
+{
+    GetCubes(numb);
+}
