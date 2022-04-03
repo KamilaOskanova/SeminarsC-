3483231e44e2017a4504a6c4810cb08c9cce3563
«Kamila» <koskanova@outlook.com>
2022-04-04 01:54:14 +0300

    Added the second task about the sum of the digits from Homework 4

---
+++ Seminar4/Homework4/Task002/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int DigitSum(int a)
+{
+    int sum = 0;
+    while (a > 0)
+    {
+        int ostatok = 0;
+        ostatok = a % 10;
+        a = a / 10;
+        sum = sum + ostatok;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите натуральное число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > 0) Console.WriteLine($"Сумма чисел равна {DigitSum(numberA)}");
+else Console.WriteLine("Вы ввели ненатуральное число. Попробуйте еще раз!");
