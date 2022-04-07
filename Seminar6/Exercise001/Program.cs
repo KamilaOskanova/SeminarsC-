312f1ec50999d9729a36e56139c654ac8a1b4126
«Kamila» <koskanova@outlook.com>
2022-04-07 15:30:02 +0300

    Added Exercise001 in Seminar6 about Triagle sides, Binary system, Fibonacci numbers and two-dimensional array

--- Seminar6/Exercise001/Program.cs
+++ Seminar6/Exercise001/Program.cs
@@ -71,12 +71,14 @@ Fibonacci(number);
 // с помощью поэлементного копирования.
 
 int[,] matrix = new int [3,4];
-for (int i = 0; i < 3; i++)
+for (int i = 0; i < matrix.GetLength(0); i++)
 {
-    for (int j = 0; j < 4; j++)
+    for (int j = 0; j < matrix.GetLength(1); j++)
     {
         matrix[i,j] = new Random().Next(0,10);
         Console.Write(matrix[i,j] + " ");
     }
     Console.WriteLine();
 }
+
+// можно также добавить getLength(...)
